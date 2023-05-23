c03f0254ffdd1da5d16daac88dc28a8c3523cfa2
Arsalan Ali <aliarsalan.dev@gmail.com>
2023-05-24 01:17:47 +0500

    publish to railway

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import path from "path"
 import eventRouter from "./routers/eventRouter.js"
 import userRouter from "./routers/userRouter.js"
 
@@ -23,9 +24,15 @@ mongoose
 app.use("/api/users", userRouter)
 app.use("/api/events", eventRouter)
 
-app.get("/", (req, res) => {
-  res.send("Server is ready")
-})
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/client/build/index.html'))
+);
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// });
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message })
