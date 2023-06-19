fa233e34e61edcae4bf69091096f68f00a418741
Arsalan Ali <itsaliarsalan@gmail.com>
2023-06-19 09:29:33 +0500

    changed db name

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }))
 
 mongoose
   .connect(process.env.MONGODB_URI, {
-    dbName: "wowdb",
+    dbName: "wowdatabase",
     retryWrites: true,
     w: "majority",
     useNewUrlParser: true,
